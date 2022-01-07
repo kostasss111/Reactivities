a518dd797069092b86ecda5a7b02303e19de1ea2
kafonins <konstantins.afonins@midis.eu>
2022-01-08 01:32:35 +0200

    End of section 10

---
+++ Application/Core/AppException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public class AppException
+    {
+        public AppException(int sttusCode, string message, string details = null)
+        {
+            SttusCode = sttusCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int SttusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
