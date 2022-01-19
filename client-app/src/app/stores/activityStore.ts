31c2683bc2d87d970d3bd1601e8de8426188d9cb
kafonins <konstantins.afonins@midis.eu>
2022-01-19 14:57:31 +0200

    End of section 21

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,9 +1,10 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable, reaction, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity, ActivityFormValues } from "../models/activity";
 import {format} from 'date-fns';
 import { store } from "./store";
 import { Profile } from "../models/profile";
+import { Pagination, PagingParams } from "../models/pagination";
 
 export default class ActivityStore {
 
@@ -13,6 +14,9 @@ export default class ActivityStore {
     editMode = false;
     loading = false;
     loadingInitial = false;
+    pagination: Pagination | null = null;
+    pagingParams = new PagingParams();
+    predicate = new Map().set('all', true);
 
     // constructor() {
     //     makeObservable(this, {
@@ -23,7 +27,59 @@ export default class ActivityStore {
     // }
     //also can make constructor autoObservable
     constructor() {
-        makeAutoObservable(this)
+        makeAutoObservable(this);
+        reaction(
+            () => this.predicate.keys(), 
+            () => {
+                this.pagingParams = new PagingParams();
+                this.activityRegistry.clear();
+                this.loadActivities();
+            }
+        );
+    }
+
+    setPagingParams = (pagingParams: PagingParams) => {
+        this.pagingParams = pagingParams;
+    }
+
+    setPredicate = (predicate: string, value:string | Date) => {
+        const resetPredicate = () => {
+            this.predicate.forEach((value, key) => {
+                if(key !== 'startDate') this.predicate.delete(key);
+            })
+        }
+        switch(predicate){
+            case 'all' :
+                resetPredicate();
+                this.predicate.set('all', true);
+                break;
+            case 'isGoing':
+                resetPredicate();
+                this.predicate.set('isGoing', true);
+                break;
+            case 'isHost':
+                resetPredicate();
+                this.predicate.set('isHost', true);
+                break;
+            case 'startDate': 
+                this.predicate.delete('startDate');
+                this.predicate.set('startDate', value);
+                break;
+        }
+    }
+
+    get axiosParams() {
+        const params = new URLSearchParams();
+        params.append('pageNumber', this.pagingParams.pageNumber.toString());
+        params.append('pageSize', this.pagingParams.pageSize.toString());
+        this.predicate.forEach((value, key) => {
+            if(key === 'startDate') {
+                params.append(key, (value as Date).toISOString());
+            } else {
+                params.append(key, value);
+            }
+        });
+        return params;
     }
 
     get activitiesByDate() {
@@ -44,20 +100,22 @@ export default class ActivityStore {
     loadActivities = async () => {
         this.loadingInitial = true;
         try {
-            const activities = await agent.Activities.list();
+            const result = await agent.Activities.list(this.axiosParams);
 
-            activities.forEach(activity => {
-                // activity.date = activity.date.split('T')[0];
-                // //this.activities.push(activity);
-                // this.activityRegistry.set(activity.id, activity);
+            result.data.forEach(activity => {
                 this.setActivity(activity);
             });
+            this.setPagination(result.pagination);
             this.setLoadingInitial(false);
 
         } catch (error) {
             console.log(error);
             this.setLoadingInitial(false);
         }
+    }
+
+    setPagination = (pagination: Pagination) => {
+        this.pagination = pagination;
     }
 
     loadActivity = async(id: string) => {
--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,16 +1,19 @@
 import { makeAutoObservable, reaction, runInAction } from "mobx";
 import agent from "../api/agent";
-import { Photo, Profile } from "../models/profile";
+import { Photo, Profile, UserActivity } from "../models/profile";
 import { store } from "./store";
 
 export default class ProfileStore {
+    currentUserProfile: Profile | null = null;
     profile: Profile | null = null;
     loadingProfile = false;
     uploading = false;
     loading = false;
     followings: Profile[] = [];
     loadingFollowings = false;
-    activeTab = 0;
+    activeTab: number = 0;
+    userActivities: UserActivity[] = [];
+    loadingActivities = false;
 
     constructor() {
         makeAutoObservable(this);
@@ -18,7 +21,7 @@ export default class ProfileStore {
         reaction(
             () => this.activeTab,
             activeTab => {
-                if(activeTab === 3 || activeTab === 4) {
+                if (activeTab === 3 || activeTab === 4) {
                     const predicate = activeTab === 3 ? 'followers' : 'following';
                     this.loadFollowings(predicate);
                 } else {
@@ -33,7 +36,7 @@ export default class ProfileStore {
     }
 
     get isCurrentUser() {
-        if(store.userStore.user && this.profile) {
+        if (store.userStore.user && this.profile) {
             return store.userStore.user.username === this.profile.username;
         }
         return false;
@@ -48,7 +51,7 @@ export default class ProfileStore {
                 this.profile = profile;
                 this.loadingProfile = false;
             })
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loadingProfile = false);
         }
@@ -60,9 +63,9 @@ export default class ProfileStore {
             const response = await agent.Profiles.uploadPhoto(file);
             const photo = response.data;
             runInAction(() => {
-                if(this.profile) {
+                if (this.profile) {
                     this.profile.photos?.push(photo);
-                    if(photo.isMain && store.userStore.user) {
+                    if (photo.isMain && store.userStore.user) {
                         store.userStore.setImage(photo.url);
                         this.profile.image = photo.url;
                     }
@@ -81,7 +84,7 @@ export default class ProfileStore {
             await agent.Profiles.setMainPhoto(photo.id);
             store.userStore.setImage(photo.url);
             runInAction(() => {
-                if(this.profile && this.profile.photos) {
+                if (this.profile && this.profile.photos) {
                     this.profile.photos.find(p => p.isMain)!.isMain = false;
                     this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
                     this.profile.image = photo.url;
@@ -89,7 +92,7 @@ export default class ProfileStore {
 
                 this.loading = false;
             })
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false);
         }
@@ -100,12 +103,12 @@ export default class ProfileStore {
         try {
             await agent.Profiles.deletePhoto(photo.id);
             runInAction(() => {
-                if(this.profile) {
+                if (this.profile) {
                     this.profile.photos = this.profile.photos?.filter(p => p.id !== photo.id);
                 }
                 this.loading = false;
             });
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false)
         }
@@ -119,7 +122,7 @@ export default class ProfileStore {
                     store.userStore.user?.displayName) {
                     store.userStore.setDisplayName(profile.displayName);
                 }
-                this.profile = {...this.profile, ...profile as Profile};
+                this.profile = { ...this.profile, ...profile as Profile };
                 this.loading = false;
             })
         } catch (error) {
@@ -128,30 +131,30 @@ export default class ProfileStore {
         }
     }
 
-    updateFollowing = async(username: string, following: boolean) => {
+    updateFollowing = async (username: string, following: boolean) => {
         this.loading = true;
         try {
             await agent.Profiles.updateFollowing(username);
             store.activityStore.updateAttendeeFollowing(username);
             runInAction(() => {
-                if(this.profile && 
-                    this.profile.username !== store.userStore.user?.username && 
+                if (this.profile &&
+                    this.profile.username !== store.userStore.user?.username &&
                     this.profile.username === username) {
                     following ? this.profile.followersCount++ : this.profile.followersCount--;
                     this.profile.following = !this.profile.following;
                 }
-                if(this.profile && this.profile.username === store.userStore.user?.username) {
+                if (this.profile && this.profile.username === store.userStore.user?.username) {
                     following ? this.profile.followingCount++ : this.profile.followingCount--;
                 }
                 this.followings.forEach(profile => {
-                    if(profile.username === username) {
+                    if (profile.username === username) {
                         profile.following ? profile.followersCount-- : profile.followersCount++;
                         profile.following = !profile.following;
                     }
                 });
                 this.loading = false;
             });
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false);
         }
@@ -165,9 +168,25 @@ export default class ProfileStore {
                 this.followings = followings;
                 this.loadingFollowings = false;
             })
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loadingFollowings = false);
+        }
+    }
+
+    loadUserActivities = async (username: string, predicate?: string) => {
+        this.loadingActivities = true;
+        try {
+            const activities = await agent.Profiles.listActivities(username, predicate!);
+            runInAction(() => {
+                this.userActivities = activities;
+                this.loadingActivities = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loadingActivities = false;
+            })
         }
     }
 }
